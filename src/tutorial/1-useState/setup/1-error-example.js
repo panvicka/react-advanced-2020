b49d9be10c559109da0899db77752d01b77038a3
panvicka <panvicka@gmail.com>
2020-10-11 11:27:05 +0200

    useState lection

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = "random";
+
+  const handleClick = () => {
+    title = "hello";
+    console.log({ title });
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        title change
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState);
+  // console.log(useState("hello"));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+  const [text, setText] = useState("randomTitle");
+
+  const handleClick = () => {
+    setText("haha");
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button type="button" className="btn" onClick={handleClick}>
+        title change
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
