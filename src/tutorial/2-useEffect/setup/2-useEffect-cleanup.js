e1ccacace78d9e114e5cc97f2ec8abd187d944fc
panvicka <panvicka@gmail.com>
2020-10-12 20:56:41 +0200

    useEffect finished

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log("add");
+    window.addEventListener("resize", checkSize);
+    return () => {
+      console.log("clean up");
+      window.removeEventListener("resize", checkSize);
+    };
+  });
+
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size}px</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [user, setUser] = useState([]);
+
+  const getUser = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    console.log(users);
+    setUser(users);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <>
+      <h3>github users</h3>
+      <ul className="users">
+        {user.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
